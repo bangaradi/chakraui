c8e38eb2d995eda7d9d4448955b98a9871e3f857
bangaradi <adityavb@cse.iitk.ac.in>
2023-02-04 22:59:11 +0530

    add socket.js

--- src/views/admin/socket.js
+++ src/views/admin/socket.js
@@ -1,4 +1,9 @@
-import io from "socket.io-client";
+const io = require("socket.io-client");
 
-const socket = io.connect("https://ede3-14-139-38-180.in.ngrok.io");
+const socket = io.connect("https://ede3-14-139-38-180.in.ngrok.io", {
+    withCredentials: true,
+    extraHeaders: {
+        "my-custom-header": "abcd"
+    }
+});
 export default socket;
