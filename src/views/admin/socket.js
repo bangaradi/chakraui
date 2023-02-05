cd0b7dbbe18cb4b13b05e56eed94afc463b289bf
bangaradi <adityavb@cse.iitk.ac.in>
2023-02-05 19:12:35 +0530

    some socket changes

--- src/views/admin/socket.js
+++ src/views/admin/socket.js
@@ -1,9 +1,20 @@
 const io = require("socket.io-client");
 
-const socket = io.connect("https://ede3-14-139-38-180.in.ngrok.io", {
-    withCredentials: true,
+const socket = io("https://fe5187b3b82082.lhr.life", {
+    // transports: ["websocket"],
+    // upgrade: false,
+    // reconnection: true,
+    // reconnectionDelay: 1000,
+    // reconnectionAttempts: 10,
+    // timeout: 10000,
+    // autoConnect: true,
+    // forceNew: true,
+    // rejectUnauthorized: false,
     extraHeaders: {
-        "my-custom-header": "abcd"
-    }
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE",
+        "Access-Control-Allow-Headers": "*",
+        
+    },
 });
 export default socket;
